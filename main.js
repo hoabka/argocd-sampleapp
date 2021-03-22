305fdb259e7c94e469aaff34b021e8c3badbef11
hoabka <nguyenhuuhoa010292@gmail.com>
2021-03-22 21:50:08 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+console.log(process.env.ENVIRONMENT || 'dev');
+
+if (process.env.ENVIRONMENT === 'prod') {	
+    process.exit(1);	
+}
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Hello GitOps World!\n');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
